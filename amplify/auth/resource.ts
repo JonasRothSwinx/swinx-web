d419019ea6e6901a09688c7ada188e0f2e245fdf
CheatingChicken <cheatingchicken@gmail.com>
2024-01-12 12:57:08 +0100

    configured basic auth
    Subject: Enabled basic authentication and updated UX enhancements
    
    Body:
    Enhanced the authentication process by establishing basic authentication
    configuration, resulting in improved security and user verification.
    Added various new required attributes to user profiles, improving user
    management and specificity. Implemented UI changes to improve the
    overall user experience, including updates to layout and styling.
    Enabled AWS-amplify UI react for a more streamlined user authentication
    experience. Concluded with some minor general code syntax and
    indentation adjustments for better readability and maintainability.
    
    Issue References:
    N/A

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -6,36 +6,52 @@ import { defineAuth } from '@aws-amplify/backend';
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
-  loginWith: {
-    email: true,
-    // add social providers
-    externalProviders: {
-      /**
-       * first, create your secrets using `amplify sandbox secret`
-       * then, import `secret` from `@aws-amplify/backend`
-       * @see https://docs.amplify.aws/gen2/deploy-and-host/sandbox-environments/features/#setting-secrets
-       */
-      // loginWithAmazon: {
-      //   clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
-      //   clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
-      // }
+    loginWith: {
+        email: { verificationEmailStyle: "LINK" },
+        // add social providers
+        externalProviders: {
+            /**
+             * first, create your secrets using `amplify sandbox secret`
+             * then, import `secret` from `@aws-amplify/backend`
+             * @see https://docs.amplify.aws/gen2/deploy-and-host/sandbox-environments/features/#setting-secrets
+             */
+            // loginWithAmazon: {
+            //   clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+            //   clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
+            // }
+        },
     },
-  },
-  /**
-   * enable multifactor authentication
-   * @see https://docs.amplify.aws/gen2/build-a-backend/auth/manage-mfa
-   */
-  // multifactor: {
-  //   mode: 'OPTIONAL',
-  //   sms: {
-  //     smsMessage: (code) => `Your verification code is ${code}`,
-  //   },
-  // },
-  userAttributes: {
-    /** request additional attributes for your app's users */
-    // profilePicture: {
-    //   mutable: true,
-    //   required: false,
+    /**
+     * enable multifactor authentication
+     * @see https://docs.amplify.aws/gen2/build-a-backend/auth/manage-mfa
+     */
+    // multifactor: {
+    //   mode: 'OPTIONAL',
+    //   sms: {
+    //     smsMessage: (code) => `Your verification code is ${code}`,
+    //   },
     // },
-  },
+    userAttributes: {
+        /** request additional attributes for your app's users */
+        // profilePicture: {
+        //   mutable: true,
+        //   required: false,
+        // },
+        givenName: {
+            required: true,
+            mutable: false,
+        },
+        familyName: {
+            required: true,
+            mutable: true,
+        },
+        profilePage: {
+            required: false,
+            mutable: true,
+        },
+        preferredUsername: {
+            required: false,
+            mutable: true,
+        },
+    },
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,103 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import config from "@/amplifyconfiguration.json";
+import "@aws-amplify/ui-react/styles.css";
+import type { WithAuthenticatorProps } from "@aws-amplify/ui-react";
+import { FetchUserAttributesOutput, fetchUserAttributes, getCurrentUser } from "aws-amplify/auth";
+import { useEffect, useState } from "react";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
+Amplify.configure(config);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <span>+</span>
-        <Image
-          src="/amplify.svg"
-          alt="Amplify Logo"
-          width={45}
-          height={37}
-          priority
-        />
-      </div>
+function Home({ signOut, user }: WithAuthenticatorProps) {
+    const [attr, setattr] = useState<FetchUserAttributesOutput>();
+    useEffect(() => {
+        fetchUserAttributes().then((result) => {
+            console.log(result);
+            setattr(result);
+        });
 
-      <div className={styles.grid}>
-        <a
-          href="https://docs.amplify.aws/gen2/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Review documentation for Amplify's code-first DX (Gen 2).</p>
-        </a>
+        return () => {};
+    }, [user]);
 
-        <a
-          href="https://docs.amplify.aws/gen2/start/quickstart/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Quickstart <span>-&gt;</span>
-          </h2>
-          <p>Follow a tutorial to build a fullstack app with Amplify Gen 2.</p>
-        </a>
+    return (
+        <main className={styles.main}>
+            <div className={styles.user}>
+                <h1>Hello {attr?.given_name ?? ""}</h1>
+                <button onClick={signOut}>Sign out</button>
+            </div>
+            <div className={styles.description}>
+                <p>
+                    Get started by editing&nbsp;
+                    <code className={styles.code}>app/page.tsx</code>
+                </p>
+            </div>
 
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/auth/set-up-auth/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Auth <span>-&gt;</span>
-          </h2>
-          <p>Zero-config Auth UI components with social sign-in and MFA.</p>
-        </a>
+            <div className={styles.center}>
+                <Image
+                    className={styles.logo}
+                    src="/next.svg"
+                    alt="Next.js Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+                <span>+</span>
+                <Image src="/amplify.svg" alt="Amplify Logo" width={45} height={37} priority />
+            </div>
 
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/data/set-up-data/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Data <span>-&gt;</span>
-          </h2>
-          <p>
-            Fully-typed real-time API with NoSQL database.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <div className={styles.grid}>
+                <a
+                    href="https://docs.amplify.aws/gen2/"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Docs <span>-&gt;</span>
+                    </h2>
+                    <p>Review documentation for Amplify's code-first DX (Gen 2).</p>
+                </a>
+
+                <a
+                    href="https://docs.amplify.aws/gen2/start/quickstart/"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Quickstart <span>-&gt;</span>
+                    </h2>
+                    <p>Follow a tutorial to build a fullstack app with Amplify Gen 2.</p>
+                </a>
+
+                <a
+                    href="https://docs.amplify.aws/gen2/build-a-backend/auth/set-up-auth/"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Auth <span>-&gt;</span>
+                    </h2>
+                    <p>Zero-config Auth UI components with social sign-in and MFA.</p>
+                </a>
+
+                <a
+                    href="https://docs.amplify.aws/gen2/build-a-backend/data/set-up-data/"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Data <span>-&gt;</span>
+                    </h2>
+                    <p>Fully-typed real-time API with NoSQL database.</p>
+                </a>
+            </div>
+        </main>
+    );
 }
+export default withAuthenticator(Home);
