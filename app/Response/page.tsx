ba96297b206f30857e842603dd0400a795ca25e7
CheatingChicken <cheatingchicken@gmail.com>
2024-03-12 15:07:50 +0100

    Refactor page components and add SearchParams component

--- app/Response/page.tsx
+++ app/Response/page.tsx
@@ -9,42 +9,49 @@ async function processResponse(candidateId: string, response: string) {
     const { errors } = await publicProcessResponse({ id: candidateId, response });
     console.log({ errors });
 }
-export default function Page() {
+function SearchParams() {
     const params = useSearchParams();
     const decodedParams = JSON.parse(atob(params.get("q") ?? ""));
     const [received, setReceived] = useState(false);
     console.log({ params, decodedParams });
     return (
+        <>
+            <h1>
+                Hallo {decodedParams.firstName ?? "Vorname"} {decodedParams.lastName ?? "Nachname"}
+            </h1>
+            <Grid2 container columns={5} rowGap={"10px"}>
+                {/* <Grid2 xs={5}>{params.toString()}</Grid2> */}
+                <Grid2 xs={1}>Assignment Id:</Grid2>
+                <Grid2 xs={4}>{decodedParams.id}</Grid2>
+                <Grid2 xs={1}>Response:</Grid2>
+                <Grid2 xs={4}>{decodedParams.response}</Grid2>
+            </Grid2>
+            <Typography whiteSpace={"pre-wrap"}>
+                {JSON.stringify(decodedParams, null, "\t")}
+            </Typography>
+
+            {received ? (
+                <Typography>Antwort erhalten</Typography>
+            ) : (
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        processResponse(decodedParams.id, decodedParams.response);
+                        setReceived(true);
+                    }}
+                >
+                    Antwort bestätigen
+                </Button>
+            )}
+        </>
+    );
+}
+
+export default function Page() {
+    return (
         <div style={{ padding: "50px" }}>
             <Suspense fallback={<div>Loading...</div>}>
-                <h1>
-                    Hallo {decodedParams.firstName ?? "Vorname"}{" "}
-                    {decodedParams.lastName ?? "Nachname"}
-                </h1>
-                <Grid2 container columns={5} rowGap={"10px"}>
-                    {/* <Grid2 xs={5}>{params.toString()}</Grid2> */}
-                    <Grid2 xs={1}>Assignment Id:</Grid2>
-                    <Grid2 xs={4}>{decodedParams.id}</Grid2>
-                    <Grid2 xs={1}>Response:</Grid2>
-                    <Grid2 xs={4}>{decodedParams.response}</Grid2>
-                </Grid2>
-                <Typography whiteSpace={"pre-wrap"}>
-                    {JSON.stringify(decodedParams, null, "\t")}
-                </Typography>
-
-                {received ? (
-                    <Typography>Antwort erhalten</Typography>
-                ) : (
-                    <Button
-                        variant="contained"
-                        onClick={() => {
-                            processResponse(decodedParams.id, decodedParams.response);
-                            setReceived(true);
-                        }}
-                    >
-                        Antwort bestätigen
-                    </Button>
-                )}
+                <SearchParams />
             </Suspense>
         </div>
     );
--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -2,14 +2,18 @@
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-export default function Page() {
+function SearchParams() {
     const params = useSearchParams();
+
     console.log(params.forEach((val, key) => console.log(key, val)));
+    return <div>{params.toString()}</div>;
+}
+export default function Page() {
     return (
         <div>
             <h1>Hello!</h1>
             <Suspense fallback={<div>Loading...</div>}>
-                <div>{params.toString()}</div>
+                <SearchParams />
             </Suspense>
         </div>
     );
