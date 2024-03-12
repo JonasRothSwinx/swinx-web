df81986b10c2fce4587daff925a01b9e7724565a
CheatingChicken <cheatingchicken@gmail.com>
2024-03-12 14:16:44 +0100

    Fix dependencies and add console log

--- amplify/functions/reminderTrigger/handler.ts
+++ amplify/functions/reminderTrigger/handler.ts
@@ -3,10 +3,10 @@ import { Handler } from "aws-lambda";
 export const handler: Handler = async (event, context) => {
     const BASE_URL = process.env.BASE_URL;
     console.log("BASE_URL", BASE_URL);
-    console.log("environment", process.env);
-    await fetch(`${BASE_URL}/remindersTrigger`, {
+    const response = await fetch(`${BASE_URL}/remindersTrigger`, {
         method: "GET",
     });
+    console.log("response", response);
     return {
         statusCode: 200,
         body: JSON.stringify("Hello from Lambda!"),
