01354f4442b28877a3a3a4a8b0c20312bf8b38a9
CheatingChicken <cheatingchicken@gmail.com>
2024-07-11 12:22:39 +0200

    chore: Refactor SwinxLogo component to use next/image for better performance

--- app/Response/Components/SwinxLogo.tsx
+++ app/Response/Components/SwinxLogo.tsx
@@ -1,12 +1,14 @@
-import { Box, SxProps } from "@mui/material";
 import Image from "next/image";
-import { useMemo } from "react";
 
 //MARK: SwinxLogo
 export function SwinxLogo() {
     return (
-        // <Box id="SwinxLogo" sx={style}>
-        <Image id="SwinxLogo" src="/swinx-logo.svg" alt="Swinx Logo" width={200} height={40} />
-        // </Box>
+        <Image
+            id="SwinxLogo"
+            src="/swinx-logo.svg"
+            alt="Swinx Logo"
+            width={200}
+            height={40}
+        />
     );
 }
